32904e38f0fcd76474df275c48afb37f7972918f
casjay <casjay@yahoo.com>
2020-08-16 01:05:22 -0400

    Update default.sh

--- scripts/default.sh
+++ scripts/default.sh
@@ -54,6 +54,11 @@ run_grub() { printf_green "Setting up grub"; rm -Rf /boot/*rescue* ; devnull gru
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 [ ! -z "$1" ] && printf_exit 'To many options provided'
+if [ -f /etc/casjaysdev/updates/versions/configs.txt ]; then
+print_red "This has already been installed"
+print_red "To reinstall please remove the version file in"
+printf_exit "/etc/casjaysdev/updates/versions/configs.txt"
+fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -83,9 +88,6 @@ printf_info "Installer version: $(retrieve_version_file)"
 
 run_external "yum clean all"
 
-run_external mkdir -p /etc/casjaysdev/updates/versions
-run_external echo "$VERSION" >/etc/casjaysdev/updates/versions/configs.txt
-run_external chmod -Rf 664 /etc/casjaysdev/updates/versions/configs.txt
 run_external rm -Rf /etc/yum.repos.d/*
 grab_remote_file https://rpm-devel.sourceforge.io/ZREPO/RHEL/7/casjay.repo -o /etc/yum.repos.d/casjay.repo
 
@@ -802,37 +804,105 @@ printf_head "Fixing packages"
 ##################################################################################################################
 
 run_grub
+rm -Rf /etc/named* /var/named/* /etc/ntp* /etc/cron*/0* /etc/cron*/dailyjobs /var/ftp/uploads /etc/httpd/conf.d/ssl.conf /tmp/configs
 
 ##################################################################################################################
 printf_head "setting up config files"
 ##################################################################################################################
 
-#run_post "cp -rT /etc/skel $HOME"
-#run_post "dotfilesreq bash"
-#run_post "dotfilesreq misc"
+git clone -q https://github.com/phpsysinfo/phpsysinfo /var/www/html/sysinfo
+git clone -q https://github.com/casjay-base/centos /tmp/configs
 
-#run_post dotfilesreqadmin samba
+find /tmp/configs -type f -iname "*.sh" -exec chmod 755 {} \;
+find /tmp/configs -type f -iname "*.pl" -exec chmod 755 {} \;
+find /tmp/configs -type f -iname "*.cgi" -exec chmod 755 {} \;
+find /tmp/configs -type f -exec sed -i "s#myserverdomainname#$(hostname -f)#g" {} \;
+find /tmp/configs -type f -exec sed -i "s#myhostnameshort#$(hostname -s)#g" {} \;
+find /tmp/configs -type f -exec sed -i "s#mydomainname#$(hostname -f | awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//')#g" {} \;
+
+#rm -Rf /tmp/configs/etc/{fail2ban,shorewall,shorewall6}
+cp -Rf /tmp/configs/{etc,root,usr,var}* /
+
+mkdir -p /etc/rsync.d /var/log/named && chown -Rf named:named /etc/named* /var/named /var/log/named
+chown -Rf apache:apache /var/www /usr/share/httpd
+
+sed -i "s#myserverdomainname#$(echo $HOSTNAME)#g" /etc/sysconfig/network
+sed -i "s#mydomain#$(echo $HOSTNAME | awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//')#g" /etc/sysconfig/network
+domainname $(hostname -f | awk -F. '{$1="";OFS="." ; print $0}' | sed 's/^.//') && echo "kernel.domainname=$(domainname)" >>/etc/sysctl.conf
+
+chmod 644 -Rf /etc/cron.d/* /etc/logrotate.d/*
+touch /etc/postfix/mydomains.pcre
+postmap /etc/postfix/transport /etc/postfix/canonical /etc/postfix/virtual /etc/postfix/mydomains && newaliases
+chattr +i /etc/resolv.conf
 
 ##################################################################################################################
 printf_head "Enabling services"
 ##################################################################################################################
 
-#system_service_enable lightdm.service
-#system_service_enable bluetooth.service
-#system_service_enable smb.service
-#system_service_enable nmb.service
-#system_service_enable avahi-daemon.service
-#system_service_enable tlp.service
-#system_service_enable org.cups.cupsd.service
-#system_service_disable mpd
+for s in $(echo sshd tor munin-node cockpit postfix uptimed php-fpm proftpd rsyslog ntpd snmpd cockpit.socket named); do 
+system_service_enable $s
+done
 
-#run_post "devnull systemctl set-default multi-user.target"
+##################################################################################################################
+printf_head "Disabling services"
+##################################################################################################################
+
+for s in $(echo firewalld chrony auditd kdump iscsid.socket iscsi iscsiuio.socket lvm2-lvmetad.socket lvm2-lvmpolld.socket lvm2-monitor mdmonitor fail2ban shorewall shorewall6 dhcpd dhcpd6 radvd); do 
+system_service_disable $s
+done
 
 ##################################################################################################################
 printf_head "Cleaning up"
 ##################################################################################################################
+system_service_enable httpd 
+system_service_enable nginx
+echo "" >/etc/yum/pluginconf.d/subscription-manager.conf
 
-#remove_pkg 
+rm -Rf /tmp/*.tar /tmp/dotfiles /tmp/configs
+/root/bin/changeip.sh >/dev/null 2>&1
+
+mkdir -p /mnt/backups /var/www/html/.well-known /etc/letsencrypt/live
+echo "" >>/etc/fstab
+#echo "10.0.254.1:/mnt/Volume_1/backups         /mnt/backups                 nfs defaults,rw 0 0" >> /etc/fstab
+#echo "10.0.254.1:/var/www/html/.well-known     /var/www/html/.well-known    nfs defaults,rw 0 0" >> /etc/fstab
+#echo "10.0.254.1:/etc/letsencrypt              /etc/letsencrypt             nfs defaults,rw 0 0" >> /etc/fstab
+#mount -a
+
+#if using letsencrypt certificates
+chmod 600 /etc/named/certbot-update.conf
+#if [[ -d /etc/letsencrypt/live/$(domainname) ]] || [[ -d /etc/letsencrypt/live/domain ]]; then
+#  ln -s /etc/letsencrypt/live/$(domainname) /etc/letsencrypt/live/domain
+#  find /etc/postfix /etc/httpd /etc/nginx -type f -exec sed -i 's#/etc/ssl/CA/CasjaysDev/certs/ca.crt#/etc/letsencrypt/live/domain/cert.pem#g' {} \;
+#  find /etc/postfix /etc/httpd /etc/nginx -type f -exec sed -i 's#/etc/ssl/CA/CasjaysDev/certs/localhost.crt#/etc/letsencrypt/live/domain/fullchain.pem#g' {} \;
+#  find /etc/postfix /etc/httpd /etc/nginx -type f -exec sed -i 's#/etc/ssl/CA/CasjaysDev/private/localhost.key#/etc/letsencrypt/live/domain/privkey.pem#g' {} \;
+if [ -f /etc/letsencrypt/live/domain/fullchain.pem ]; then
+cat /etc/letsencrypt/live/domain/fullchain.pem >/etc/cockpit/ws-certs.d/1-my-cert.cert
+cat /etc/letsencrypt/live/domain/privkey.pem >>/etc/cockpit/ws-certs.d/1-my-cert.cert
+fi
+#else
+#If using self-signed certificates
+#  find /etc/postfix /etc/httpd /etc/cockpit/ws-certs.d -type f -exec sed -i 's#/etc/letsencrypt/live/domain/cert.pem#/etc/ssl/CA/CasjaysDev/certs/ca.crt#g' {} \;
+#  find /etc/postfix /etc/httpd /etc/cockpit/ws-certs.d -type f -exec sed -i 's#/etc/letsencrypt/live/domain/fullchain.pem#/etc/ssl/CA/CasjaysDev/certs/localhost.crt#g' {} \;
+#  find /etc/postfix /etc/httpd /etc/cockpit/ws-certs.d -type f -exec sed -i 's#/etc/letsencrypt/live/domain/privkey.pem#/etc/ssl/CA/CasjaysDev/private/localhost.key#g' {} \;
+#fi
+
+update-ca-trust && update-ca-trust extract
+
+bash -c "$(munin-node-configure --remove-also --shell >/dev/null 2>&1)"
+
+if [ -f /var/lib/tor/hidden_service/hostname ]; then 
+cp -Rfv /var/lib/tor/hidden_service/hostname /var/www/html/tor_hostname
+fi
+
+chown -Rf apache:apache /var/www
+systemctl restart munin-node
+history -c && history -w
+
+##################################################################################################################
+
+run_external mkdir -p /etc/casjaysdev/updates/versions
+run_external echo "$VERSION" >/etc/casjaysdev/updates/versions/configs.txt
+run_external chmod -Rf 664 /etc/casjaysdev/updates/versions/configs.txt
 
 ##################################################################################################################
 printf_head "Finished " ; echo""
